5a71257e91ae1ea366b99ba7c80e875c4bce32d8
Muchammad Ramadhan <muchammadramadhan12@gmail.com>
2024-03-20 02:34:00 +0700

    pembaruan

--- src/Component/HalamanUtama.jsx
+++ src/Component/HalamanUtama.jsx
@@ -2,10 +2,12 @@ import Logo from "../assets/logo.png";
 import Footer from "./Footer";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { useAuth } from "../Context/AuthContext";
 export default function HalamanUtama() {
+  const { logout } = useAuth();
   const [currentSlide, setCurrentSlide] = useState(1);
   const data = JSON.parse(localStorage.getItem("DataUser"));
-   useEffect(() => {
+  useEffect(() => {
     const interval = setInterval(() => {
       setCurrentSlide((prevSlide) => (prevSlide % 3) + 1);
     }, 2000);
@@ -17,6 +19,12 @@ export default function HalamanUtama() {
       <div className="container-page">
         <div className="logo-page">
           <img src={Logo} alt="logo" />
+          <div className="logout" onClick={() => logout()}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
+              <path d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z" />
+              <path d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z" />
+            </svg>
+          </div>
         </div>
         <div className="bg-motif">
           <div className="box-customer">
